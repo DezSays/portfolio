3b7fb5073957bde26060d493f09f8a6acb73ad09
Dezarea Bryan <dezareabryan@gmail.com>
2023-06-26 23:34:33 -0400

    blog styling

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -4,6 +4,9 @@ const blogContainer = document.getElementById('blog-container');
 const blogCard = document.createElement('div');
 blogCard.classList.add('blog-card');
 
+const blogImageContainer = document.createElement('div');
+blogImageContainer.classList.add('blog-image-container');
+
 const blogImage = document.createElement('img');
 blogImage.classList.add('blog-image');
 blogImage.src = content.img;
@@ -12,6 +15,10 @@ blogImage.alt = `${content.title}`;
 const blogTitle = document.createElement('h3');
 blogTitle.classList.add('blog-title');
 blogTitle.textContent = content.title;
+
+const blogDescription = document.createElement('p');
+blogDescription.classList.add('blog-description');
+blogDescription.textContent = content.description;
 
 const readMoreButton = document.createElement('button');
 readMoreButton.classList.add('read-more-button');
@@ -22,7 +29,10 @@ readMoreButton.addEventListener('click', () => {
   window.open(content.url, '_blank');
 });
 
-blogCard.appendChild(blogImage);
+blogImageContainer.appendChild(blogImage);
+blogImageContainer.appendChild(blogDescription);
+
+blogCard.appendChild(blogImageContainer);
 blogCard.appendChild(blogTitle);
 blogCard.appendChild(readMoreButton);
 
