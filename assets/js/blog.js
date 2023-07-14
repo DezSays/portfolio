9b4f1cc00441773e053011a5f38301e7a910529c
Dezarea Bryan <dezareabryan@gmail.com>
2023-07-13 23:42:09 -0400

    added blogs and formatted files. Needs to update hosting links for projects.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,39 +1,44 @@
-import content from './blogNavs.js';
-const blogContainer = document.getElementById('blog-container');
+import content from "./blogNavs.js";
 
-const blogCard = document.createElement('div');
-blogCard.classList.add('blog-card');
+document.addEventListener("DOMContentLoaded", () => {
+  const blogContainer = document.getElementById("blog-container");
 
-const blogImageContainer = document.createElement('div');
-blogImageContainer.classList.add('blog-image-container');
+  content.forEach((item) => {
+    const blogCard = document.createElement("div");
+    blogCard.classList.add("blog-card");
 
-const blogImage = document.createElement('img');
-blogImage.classList.add('blog-image');
-blogImage.src = content.img;
-blogImage.alt = `${content.title}`;
+    const blogImageContainer = document.createElement("div");
+    blogImageContainer.classList.add("blog-image-container");
 
-const blogTitle = document.createElement('h3');
-blogTitle.classList.add('blog-title');
-blogTitle.textContent = content.title;
+    const blogImage = document.createElement("img");
+    blogImage.classList.add("blog-image");
+    blogImage.src = item.img;
+    blogImage.alt = item.title;
 
-const blogDescription = document.createElement('p');
-blogDescription.classList.add('blog-description');
-blogDescription.textContent = content.description;
+    const blogTitle = document.createElement("h3");
+    blogTitle.classList.add("blog-title");
+    blogTitle.textContent = item.title;
 
-const readMoreButton = document.createElement('button');
-readMoreButton.classList.add('read-more-button');
-readMoreButton.textContent = 'Read More';
+    const blogDescription = document.createElement("p");
+    blogDescription.classList.add("blog-description");
+    blogDescription.textContent = item.description;
 
-// Add event listener to open the blog URL in a new tab
-readMoreButton.addEventListener('click', () => {
-  window.open(content.url, '_blank');
+    const readMoreButton = document.createElement("button");
+    readMoreButton.classList.add("read-more-button");
+    readMoreButton.textContent = "Read More";
+
+    // Add event listener to open the blog URL in a new tab
+    readMoreButton.addEventListener("click", () => {
+      window.open(item.url, "_blank");
+    });
+
+    blogImageContainer.appendChild(blogImage);
+    blogImageContainer.appendChild(blogDescription);
+
+    blogCard.appendChild(blogImageContainer);
+    blogCard.appendChild(blogTitle);
+    blogCard.appendChild(readMoreButton);
+
+    blogContainer.appendChild(blogCard);
+  });
 });
-
-blogImageContainer.appendChild(blogImage);
-blogImageContainer.appendChild(blogDescription);
-
-blogCard.appendChild(blogImageContainer);
-blogCard.appendChild(blogTitle);
-blogCard.appendChild(readMoreButton);
-
-blogContainer.appendChild(blogCard);
--- assets/js/blogNavs.js
+++ assets/js/blogNavs.js
@@ -1,8 +1,19 @@
-export default {
-    "id": 1,
-    "title": "Cracking the Legacy Code: Unveiling the Importance of Understanding Legacy Code",
-    "description": "Discover why understanding legacy code is a game-changer for web developers. Unveil the benefits it brings, from minimizing risks to enhancing professional growth, in this captivating article.",
-    "img": "images/Cracking-the-Legacy-Code.jpg",
-    "url": "https://medium.com/@dezareabryan_39588/2b5a9d6b1361"
-  };
-  
+export default [
+  {
+    id: 1,
+    title:
+      "Cracking the Legacy Code: Unveiling the Importance of Understanding Legacy Code",
+    description:
+      "Discover why understanding legacy code is a game-changer for web developers. Unveil the benefits it brings, from minimizing risks to enhancing professional growth, in this captivating article.",
+    img: "images/Cracking-the-Legacy-Code.jpg",
+    url: "https://medium.com/@dezareabryan_39588/2b5a9d6b1361",
+  },
+  {
+    id: 2,
+    title: "ElephantSQL: Simplifying PostgreSQL Database Management",
+    description:
+      "Simplify PostgreSQL database management with ElephantSQL, a user-friendly and scalable cloud-based service.",
+    img: "images/elephantsql.png",
+    url: "https://medium.com/@dezareabryan_39588/elephantsql-simplifying-postgresql-database-management-7fb0c6bebe36",
+  },
+];
