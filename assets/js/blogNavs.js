c749a527512c98a3ba3fc6e9321db6233dd4b6a2
Dezarea Bryan <dezareabryan@gmail.com>
2023-07-15 09:56:08 -0400

    added up to id #8 for blogs. Will continue working on schedule.

--- assets/js/blogNavs.js
+++ assets/js/blogNavs.js
@@ -16,4 +16,52 @@ export default [
     img: "images/elephantsql.png",
     url: "https://medium.com/@dezareabryan_39588/elephantsql-simplifying-postgresql-database-management-7fb0c6bebe36",
   },
+  {
+    id: 3,
+    title: "Demystifying the Differences Between Margin and Padding in Web Design",
+    description:
+      "Explore the distinctions between margin and padding, learn their purposes, and gain practical tips for leveraging these CSS properties to enhance spacing and organization in your web layouts.",
+    img: "images/padding-and-margin.jpg",
+    url: "https://medium.com/@dezareabryan_39588/demystifying-the-differences-between-margin-and-padding-in-web-design-87005f9ab73e",
+  },
+  {
+    id: 4,
+    title: "Mastering CSS Selectors: A Comprehensive Guide for Beginners",
+    description:
+      "** COMING JULY 19th ** Mastering CSS Selectors: A comprehensive guide for beginners, empowering you with the knowledge and confidence to wield the power of CSS selectors in web development.",
+    img: "images/css-selectors.jpg",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "Exploring the Power of PostgreSQL: A Beginner's Guide to Versatile Database Management",
+    description:
+      "** COMING JULY 26th ** Explore the power of PostgreSQL, a versatile and feature-rich open-source database management system, through this beginner's guide, uncovering its capabilities and real-world applications.",
+    img: "images/database.jpg",
+    url: "#",
+  },
+  {
+    id: 6,
+    title: "Unleashing the Power of Spatial Databases: Navigating Location-Based Data",
+    description:
+      "** COMING AUGUST 2nd ** Unleash the power of spatial databases, navigate location-based data, and unlock hidden insights for informed decision-making.",
+    img: "images/spatial.png",
+    url: "#",
+  },
+  {
+    id: 7,
+    title: "Relational Databases: Unleashing the Power of Structured Data",
+    description:
+      "** COMING AUGUST 9th ** Explore the significance of relational databases through examples and practical tips for maximum effectiveness.",
+    img: "images/db.jpg",
+    url: "#",
+  },
+  {
+    id: 8,
+    title: "Time-Series Databases: Unleashing the Power of Time-Stamped Data",
+    description:
+      "** COMING AUGUST 16th ** Unlock the power of time-stamped data with time-series databases, enabling efficient storage, analysis, and decision-making based on temporal behavior, as explored in this captivating blog post.",
+    img: "images/time-series-database.jpg",
+    url: "#",
+  },
 ];
